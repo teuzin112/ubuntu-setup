21af2da211ada3c97a747f60d119a63ee5a588d8
Matheus Andrade <58920425+teuzin112@users.noreply.github.com>
2025-01-09 09:16:23 -0300

    Create pyenv_install.sh

---
+++ pyenv_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+export PATH="/home/$USER/.pyenv/bin:$PATH"
+eval "$(/home/$USER/.pyenv/bin/pyenv init -)"
+eval "$(/home/$USER/.pyenv/bin/pyenv virtualenv-init -)"
+echo '######## Installing Python 3.9.0 and set as global #######'
+echo '##########################################################'
+echo '     ###############################################'
+username=$(logname)
+pyenv install 3.12.0
+pyenv global 3.12.0
